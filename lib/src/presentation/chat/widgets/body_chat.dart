47c7bb7a3d5bc164a48f001febb846228a10e1c1
albint3r <albint3r@gmail.com>
2023-06-24 17:00:12 -0600

    Checkpoint

--- lib/src/presentation/chat/widgets/body_chat.dart
+++ lib/src/presentation/chat/widgets/body_chat.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 import '../../core/widgets/app_close_bar.dart';
-import 'chat_text_field.dart';
-import 'message_box.dart';
 import 'chat_container.dart';
+import 'chat_text_field.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_channel/status.dart' as status;
+
+
 
 class BodyChat extends StatelessWidget {
   const BodyChat({
